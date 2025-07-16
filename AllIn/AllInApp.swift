1972d1ab9abb14737be39f8c4bb0b26c53083737
blackcode2 <doky03115@gmail.com>
2025-07-17 01:34:46 +0900

    Initial Commit

---
+++ AllIn/AllInApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllInApp.swift
+//  AllIn
+//
+//  Created by KimDogyung on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct AllInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllInTests/AllInTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllInTests.swift
+//  AllInTests
+//
+//  Created by KimDogyung on 7/17/25.
+//
+
+import Testing
+@testable import AllIn
+
+struct AllInTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
